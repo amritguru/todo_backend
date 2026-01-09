15b280589bf0445ff57abe58b4d12dd1d82a3e67
Mantu Kumar ( Amrit ) <amritguru.work@gmail.com>
2026-01-10 01:04:32 +0530

    Modify CORS settings to allow all origins
    Updated CORS middleware to allow requests from any origin.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
@@ -13,3 +15,4 @@ app.use("/api/todos", todoRoutes);
 app.use(errorHandler);
 
 module.exports = app;
+
